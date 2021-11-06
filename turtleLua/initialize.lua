95b131d4159576be69fe9983b478e018847c3814
Adam Cooper <frankidyfrank3@Gmail.com>
2021-11-05 21:38:04 -0500

    yeet

---
+++ turtleLua/initialize.lua
@@ -0,0 +1,3 @@
+shell.run("pastebin", "get", "4nRg9CHU", "json")
+shell.run("wget", "https://raw.githubusercontent.com/Coop25/Minecraft-Turtle-Control/master/turtleLua/index.lua", "startup")
+shell.run("reboot")
