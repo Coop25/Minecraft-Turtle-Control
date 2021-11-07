b2f23cdf6faeb7435aa904975ed779b017a87a34
Adam Cooper <frankidyfrank3@Gmail.com>
2021-11-07 08:49:37 -0600

    updates

--- turtleLua/utils.lua
+++ turtleLua/utils.lua
@@ -2,13 +2,22 @@ local utils = {}
 
 function utils.tunnel(length) 
     for i=1, length do
-        turtle.dig()
-        turtle.digUp()
+        while turtle.detect() do
+            turtle.dig()
+        end
         turtle.forward()
+        turtle.digUp()
     end
+    turtle.turnLeft()
+    turtle.turnLeft()
     for i=1, length do
+        while turtle.detect() do
+            turtle.dig()
+        end
         turtle.back()
     end
+    turtle.turnLeft()
+    turtle.turnLeft()
     return
 end
 
